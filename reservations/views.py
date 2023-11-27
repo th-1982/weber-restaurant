1151a7696695e80713a06ed05f6e2b34f2c902be
Theresa Wolff <theresawolff4@gmail.com>
2023-11-27 14:15:47 +0000

    Add views for the home page, menu page, contact page, booking page

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import ReservationForm
 
 # Create your views here.
+
+
+def homepage_view(request):
+    return render(request, "reservations/homepage.html")
+
+def contact_view(request):
+    return render(request, "reservations/contact.html")
+
+def menu_view(request):
+    return render(request, "reservations/menu.html")
+
+def booking_view(request):
+    return render(request, "reservations/booking.html")
