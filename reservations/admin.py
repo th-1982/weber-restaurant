dcfad37b37113893336d9b6a6e79f0043bb4e950
Theresa Wolff <theresawolff4@gmail.com>
2023-11-27 11:51:25 +0000

    Add the setting up of admin panel

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Reservations, Table 
 
 # Register your models here.
+
+admin.site.register(Table)
+
+@admin.register(Reservations)
+class ReservationsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'date', 'time', 'notes', 'number_of_guests']
+
