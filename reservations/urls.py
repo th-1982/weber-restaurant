caaeafb76bb240f7357ccdbece63352194b33097
Theresa Wolff <theresawolff4@gmail.com>
2023-12-05 00:32:27 +0000

    Add URL pattern for Reservation_List_View

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from .views import homepage_view, menu_view, booking_view, contact_view
-from .views import HomePageView, MenuView, BookingView, ContactView
+from .views import HomePageView, MenuView, BookingView, ContactView, Reservation_List_View
 
 
 
@@ -10,6 +10,6 @@ urlpatterns = [
     path('contact/', ContactView.as_view(), name='contact'),
     path('menu/', MenuView.as_view(), name='menu'),
     path('booking/', BookingView.as_view(), name='booking'),
-
+    path('reservation_list', Reservation_List_View.as_view(), name='reservation_list'),
 
 ]
