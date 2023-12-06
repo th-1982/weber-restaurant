2937598d4bc2ab41cd3b55123ce2c24b47141f24
Theresa Wolff <theresawolff4@gmail.com>
2023-12-06 00:36:14 +0000

    Add Crispy Forms tag

--- reservations/forms.py
+++ reservations/forms.py
@@ -6,4 +6,4 @@ class ReservationForm(forms.ModelForm):
     date = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
     class Meta:
         model = Reservations
-        fields = ('user', 'customer_email','name', 'date', 'time', 'notes', 'number_of_guests', 'table')
+        fields = ('user', 'customer_email','name', 'date', 'time', 'notes', 'number_of_guests', 'table',)
