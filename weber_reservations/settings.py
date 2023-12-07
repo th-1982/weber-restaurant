a1dcb74666ed4ef7aa86189311087e02b0013a26
Theresa Wolff <theresawolff4@gmail.com>
2023-12-07 00:38:05 +0000

    Update message tag in base.html

--- weber_reservations/settings.py
+++ weber_reservations/settings.py
@@ -205,3 +205,8 @@ STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-th1982-weberrestaurant-chauhj8ruql.ws-eu106.gitpod.io'
+]
